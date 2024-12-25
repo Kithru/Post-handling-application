6a3f4620887e7ec16d88cfcbe3dafdfb5fab3b24
Kithru Viduranga <kithruviduranga@gmail.com>
2024-12-25 22:14:12 +0530

    add duplicate checking function

--- Classes/Classes.php
+++ Classes/Classes.php
@@ -43,6 +43,19 @@ class Classmanager {
         }
     }  
 
+    public function checkduplicate($email,$password) {
+        $con = DBConnect::getConnection();
+
+        $query = "SELECT * FROM `user` WHERE `email`='$email' LIMIT 1";
+        $results = mysql_query($query, $con) or die(mysql_error());
+        $count=mysql_num_rows($results);
+            if($count>0){
+                    return 1;
+            }else{
+                return 0;
+            }
+    }
+
 }
 
 
